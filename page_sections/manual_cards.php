9d7287b00c7efdd5e32c851674ca9e5d5c324e60
Werner Glinka <werner@glinka.co>
2024-04-24 14:22:50 -0500

    wip

--- page_sections/manual_cards.php
+++ page_sections/manual_cards.php
@@ -12,20 +12,45 @@
   $ctas = isset($props['ctas']) && is_array($props['ctas']) ? $props['ctas'] : [];
   $hasCTAs = count($ctas) > 0;
   $is_horizontal = isset($props['is_horizontal']) && $props['is_horizontal'] ? true : false;
-
-  //echo "<pre>";
-  //print_r($cards);
-  //echo "</pre>";
-
-
 ?>
 
-<?php if ($is_horizontal): ?>
-  
-  <div class="section-text">
+<div class="container">
+  <?php if ($is_horizontal): ?>
+    
+    <div class="section-text">
+      <div class="section-header">
+        <?php render_text_component($text); ?>
+      </div>
+
+      <?php if ($hasCTAs): ?>
+        <div class="ctas-container">
+        <?php foreach ($ctas as $cta): ?>
+          <?php render_cta_component($cta);?>
+        <?php endforeach;?>
+        </div>
+      <?php endif;?>
+    </div>
+
+    <?php $length = count($cards);?>
+    <ul class="manual-cards-list items<?php echo $length ?>">
+      <?php foreach ($cards as $card): ?>
+        <?php render_manual_card_component($card);?>
+      <?php endforeach;?>
+    </ul>
+    
+
+  <?php else: ?>
+    
     <div class="section-header">
       <?php render_text_component($text); ?>
     </div>
+
+    <?php $length = count($cards);?>
+    <ul class="manual-cards-list items<?php echo $length ?>">
+      <?php foreach ($cards as $card): ?>
+        <?php render_manual_card_component($card); ?>
+      <?php endforeach; ?>
+    </ul>
 
     <?php if ($hasCTAs): ?>
       <div class="ctas-container">
@@ -34,34 +59,5 @@
       <?php endforeach;?>
       </div>
     <?php endif;?>
-  </div>
-
-  <?php $length = count($cards);?>
-  <ul class="manual-cards-list items<?php echo $length ?>">
-    <?php foreach ($cards as $card): ?>
-      <?php render_manual_card_component($card);?>
-    <?php endforeach;?>
-  </ul>
-  
-
-<?php else: ?>
-  
-  <div class="section-header">
-    <?php render_text_component($text); ?>
-  </div>
-
-  <?php $length = count($cards);?>
-  <ul class="manual-cards-list items<?php echo $length ?>">
-    <?php foreach ($cards as $card): ?>
-      <?php render_manual_card_component($card); ?>
-    <?php endforeach; ?>
-  </ul>
-
-  <?php if ($hasCTAs): ?>
-    <div class="ctas-container">
-    <?php foreach ($ctas as $cta): ?>
-      <?php render_cta_component($cta);?>
-    <?php endforeach;?>
-    </div>
-  <?php endif;?>
-<?php endif; ?>
+  <?php endif; ?>
+</div>
