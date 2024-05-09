cd9d74ecb88dcc981b4e3ea04baefaab7519c425
Werner Glinka <werner@glinka.co>
2024-05-09 16:44:41 -0500

    wip

--- page_sections/inc/section_components.php
+++ page_sections/inc/section_components.php
@@ -693,7 +693,34 @@ function render_isotope_gallery($images_gallery)
     return $output;
 }
 
+/**
+ * Render a marquee component
+ */
+function render_marquee($marquee)
+{
+    $output = "<div class='enable-animation'>";
+    $output .= "<div class='marquee'>";
 
-  
-  
+    $output .= "<ul class='marquee-content'>";
+    foreach ($marquee as $image) {
+        $image_src = wp_get_attachment_image_src($image['id'], 'large');
+        $image_src = $image_src[0];
+
+        $output .= "<li><img src='{$image_src}' alt='{$image['alt_text']}' /></li>";
+    }
+    $output .= "</ul>";
+    $output .= "<ul class='marquee-content' aria-hidden='true'>";
+    foreach ($marquee as $image) {
+        $image_src = wp_get_attachment_image_src($image['id'], 'large');
+        $image_src = $image_src[0];
+
+        $output .= "<li><img src='{$image_src}' alt='{$image['alt_text']}' /></li>";
+    }
+    $output .= "</ul>";
+
+    $output .= "</div>";
+    $output .= "</div>";
+
+    return $output;
+} 
 ?>
---
+++ section_blocks/images_marquee.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Renders a marquee
+ * It is called in flex_section via:
+ * $component_name = $component['acf_fc_layout'];
+ * get_template_part('section_blocks/' . $component_name, null, $component);
+ * where $args is passing in the $component array
+ *
+ * @package square1
+ */
+include_once get_template_directory() . '/page_sections/inc/section_components.php';
+
+//echo '<pre>';
+//print_r($args);
+//echo '</pre>';
+
+echo render_marquee($args['images_marquee']);
